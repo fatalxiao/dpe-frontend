ae0181b29b752be71a4608c6303410d096745b2a
Liangxiaojun <fatalxiao@163.com>
2018-01-13 21:38:35 +0800

    update nav

---
+++ src/apis/app/modules/patientList/PatientListApi.js
@@ -0,0 +1,19 @@
+import config from 'src/config';
+import Api from 'apis/Api';
+import RequestManagement from 'apis/RequestManagement';
+
+export default {
+
+    getPatients(options) {
+
+        const name = 'patient/getPatients';
+        RequestManagement.cancelByName(name);
+
+        Api.get({
+            ...options,
+            name,
+            url: `${config.appBaseUrl}/getPatients`
+        });
+
+    }
+};
