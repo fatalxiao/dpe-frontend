362910046e371f21e139a718babe193d23d1a705
Liangxiaojun <fatalxiao@163.com>
2018-01-13 13:30:51 +0800

    add Nav

---
+++ src/containers/app/nav/Nav.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import * as actions from 'reduxes/actions';
+
+import NavMenu from './menu/NavMenu';
+
+import 'scss/containers/app/nav/Nav.scss';
+
+class Nav extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="nav">
+
+                <NavMenu/>
+
+            </div>
+        );
+    }
+}
+
+Nav.propTypes = {};
+
+function mapStateToProps(state, ownProps) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
