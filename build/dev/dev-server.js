6e68fbc0bc0c64a843e79f657ae5d4d7f4a9d85d
Liangxiaojun <fatalxiao@163.com>
2018-01-13 13:20:04 +0800

    update dev server

--- build/dev/dev-server.js
+++ build/dev/dev-server.js
@@ -1,15 +1,20 @@
 delete process.env['DEBUG_FD'];
 
 const config = require('../../config');
-if (!process.env.NODE_ENV) {
-    process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
-}
+
+process.env.NODE_ENV = config.dev.env.NODE_ENV;
 
 const opn = require('opn'),
     webpack = require('webpack'),
+    proxyMiddleware = require('http-proxy-middleware'),
+    history = require('connect-history-api-fallback'),
+    utils = require('../utils'),
 
     port = process.env.PORT || config.dev.port,
     uri = 'http://localhost:' + port,
+
+    // API 转发配置
+    proxyTable = config.dev.proxyTable,
 
     express = require('express'),
     app = express(),
@@ -26,6 +31,7 @@ const opn = require('opn'),
         log: console.log
     });
 
+// html 模板改变时刷新页面
 compiler.plugin('compilation', compilation => {
     compilation.plugin('html-webpack-plugin-after-emit', (data, cb) => {
         hotMiddleware.publish({action: 'reload'});
@@ -33,9 +39,40 @@ compiler.plugin('compilation', compilation => {
     });
 });
 
+// 转发 API 请求
+Object.keys(proxyTable).forEach(context => {
+
+    let options = proxyTable[context];
+
+    if (typeof options === 'string') {
+        options = {
+            target: options,
+            changeOrigin: true
+        };
+    }
+
+    options.onProxyReq = (proxyReq, req, res) => {
+
+        const ip = utils.getClientIp(req);
+        ip && proxyReq.setHeader('ip', utils.ipParse(ip));
+
+        if (req.headers && !req.headers.token && req.query && req.query.token) {
+            proxyReq.setHeader('token', req.query.token);
+        }
+
+    };
+
+    app.use(proxyMiddleware(options.filter || context, options));
+
+});
+
+// browserHistory 前端路由重定向
+app.use(history());
+
 app.use(devMiddleware);
 app.use(hotMiddleware);
 
+// 托管静态文件
 app.use(config.dev.assetsVirtualRoot, express.static('./static'));
 
 devMiddleware.waitUntilValid(() => {
