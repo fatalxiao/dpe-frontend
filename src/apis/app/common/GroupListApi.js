0d52dd829004b7acad9acb5e77eddd3f3d3ce8f1
Liangxiaojun <fatalxiao@163.com>
2018-01-14 15:19:43 +0800

    update group api

---
+++ src/apis/app/common/GroupListApi.js
@@ -0,0 +1,13 @@
+import config from 'src/config';
+import Api from 'apis/Api';
+
+export default {
+
+    getGroups(options) {
+        Api.get({
+            ...options,
+            url: `${config.appBaseUrl}/group/getGroups`,
+            cancelable: false
+        });
+    }
+};
