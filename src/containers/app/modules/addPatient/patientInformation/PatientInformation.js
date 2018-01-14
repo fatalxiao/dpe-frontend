ad985bf864397f2a6c4e65cd3d29aa241923f4c1
Liangxiaojun <fatalxiao@163.com>
2018-01-14 13:01:05 +0800

    update PatientInformation

--- src/containers/app/modules/addPatient/patientInformation/PatientInformation.js
+++ src/containers/app/modules/addPatient/patientInformation/PatientInformation.js
@@ -2,11 +2,8 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {renderRoutes} from 'react-router-config';
 
 import * as actions from 'reduxes/actions/index';
-
-import RoundStep from 'alcedo-ui/RoundStep';
 
 import 'scss/containers/app/modules/addPatient/patientInformation/PatientInformation.scss';
 
@@ -17,7 +14,6 @@ class PatientInformation extends Component {
     }
 
     render() {
-
         return (
             <div className="patient-information">
 
