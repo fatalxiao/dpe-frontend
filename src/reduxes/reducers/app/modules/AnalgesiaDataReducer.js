bd392408619c046655937f9f75ba06db77e1b435
Liangxiaojun <fatalxiao@163.com>
2018-01-16 11:20:29 +0800

    update AnalgesiaData

--- src/reduxes/reducers/app/modules/AnalgesiaDataReducer.js
+++ src/reduxes/reducers/app/modules/AnalgesiaDataReducer.js
@@ -4,11 +4,20 @@ import Valid from 'vendors/Valid';
 
 const initialState = {
     steps: [{
-        title: 'Patient\nInformation'
-    }, {
-        title: 'Analgesia\nData'
-    }, {
-        title: 'Observal\nData'
+        id: 0,
+        patientId: 0,
+        timePoint: 0,
+        hasContraction: false,
+        vasScore: 10,
+        thoracicSensoryBlockLeft: 1,
+        thoracicSensoryBlockRight: 1,
+        sacralSensoryBlockLeft: 1,
+        sacralSensoryBlockRight: 1,
+        bromageScore: 10,
+        systolicBloodPressure: 80,
+        diastolicBloodPressure: 120,
+        heartRate: 70,
+        pulseOxygenSaturation: 10
     }]
 };
 
