c57df5bd146f621927a542df99a71d7be355c99c
Liangxiaojun <fatalxiao@163.com>
2018-01-18 13:59:00 +0800

    update

--- src/containers/app/nav/bar/NavBar.js
+++ src/containers/app/nav/bar/NavBar.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-
-import * as actions from 'reduxes/actions';
 
 import NavBarTop from './NavBarTop';
 import NavBarBottom from './NavBarBottom';
 
 import 'scss/containers/app/nav/bar/NavBar.scss';
 
-class NavBar extends Component {
+export default class NavBar extends Component {
 
     constructor(props) {
         super(props);
@@ -25,15 +20,3 @@ class NavBar extends Component {
         );
     }
 }
-
-NavBar.propTypes = {};
-
-function mapStateToProps(state, ownProps) {
-    return {};
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actions, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
