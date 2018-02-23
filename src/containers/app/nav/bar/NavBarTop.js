06ce72cd52927d35b1282eceac33c6fa71df2639
Liangxiaojun <fatalxiao@163.com>
2018-02-23 15:50:40 +0800

    update

--- src/containers/app/nav/bar/NavBarTop.js
+++ src/containers/app/nav/bar/NavBarTop.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import classNames from 'classnames';
 
 import * as actions from 'reduxes/actions';
 
@@ -32,24 +33,27 @@ class NavBarTop extends Component {
 
     render() {
 
-        const {children} = this.props;
+        const {children, isFold} = this.props,
+
+            className = classNames('nav-bar-top', {
+                fold: isFold
+            });
 
         return (
-            <div className="nav-bar-top">
+            <div className={className}>
 
                 <IconButton className="nav-bar-item nav-bar-logo-button"
-                            iconCls="icon-circle"
                             onTouchTap={this.goToLanding}>
+                    <div className="logo"/>
+                    <div className="logo-animated"/>
                 </IconButton>
 
                 <IconButton className="nav-bar-item"
-                            iconCls="icon-magnifying-glass">
-                </IconButton>
+                            iconCls="icon-magnifying-glass"/>
 
                 <IconButton className="nav-bar-item"
                             iconCls="icon-plus"
-                            onTouchTap={this.addPatient}>
-                </IconButton>
+                            onTouchTap={this.addPatient}/>
 
                 {children}
 
@@ -60,7 +64,11 @@ class NavBarTop extends Component {
 }
 
 NavBarTop.propTypes = {
+
+    isFold: PropTypes.bool,
+
     routerPush: PropTypes.func
+
 };
 
 function mapStateToProps(state, ownProps) {
