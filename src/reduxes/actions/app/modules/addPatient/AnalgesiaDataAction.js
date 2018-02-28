bc8696b12892fe9e651f11e8893c440327339bc3
Liangxiaojun <fatalxiao@163.com>
2018-02-28 13:12:08 +0800

    update

--- src/reduxes/actions/app/modules/addPatient/AnalgesiaDataAction.js
+++ src/reduxes/actions/app/modules/addPatient/AnalgesiaDataAction.js
@@ -1,4 +1,6 @@
 import * as actionTypes from 'reduxes/actionTypes/index';
+import {routerPush} from '../../../common/RouterAction';
+import AnalgesiaApi from 'apis/app/modules/addPatient/AnalgesiaApi';
 
 export const updateAnalgesiaDataField = (id, fieldName, fieldValue) => ({
     type: actionTypes.UPDATE_ANALGESIA_DATA_FIELD,
@@ -6,3 +8,44 @@ export const updateAnalgesiaDataField = (id, fieldName, fieldValue) => ({
     fieldName,
     fieldValue
 });
+
+export const createOrUpdateAnalgesiaData = () => (dispatch, getState) => {
+
+    const data = getState().patientInformation.form,
+        id = data.id;
+
+    if (!data.groupId || !id || !data.patientName) {
+        return;
+    }
+
+    return dispatch({
+        [actionTypes.CALL_API]: {
+            types: [
+                actionTypes.UPDATE_ANALGESIA_DATA_REQUEST,
+                actionTypes.UPDATE_ANALGESIA_DATA_SUCCESS,
+                actionTypes.UPDATE_ANALGESIA_DATA_FAILURE
+            ],
+            api: AnalgesiaApi.createOrUpdateAnalgesiaData,
+            params: {
+                groupId: data.groupId,
+                id,
+                patientName: data.patientName,
+                age: data.age,
+                gestationalDays: data.gestationalDays,
+                height: data.height,
+                weight: data.weight,
+                heartRate: data.heartRate,
+                initialVasScore: data.initialVasScore,
+                cervicalDilationAtTimeOfEA: data.cervicalDilationAtTimeOfEA,
+                systolicBloodPressure: data.systolicBloodPressure,
+                diastolicBloodPressure: data.diastolicBloodPressure,
+                foetalHeartRate: data.foetalHeartRate,
+                description: data.description
+            },
+            successCallback() {
+                routerPush(`/app/add-patient/analgesia-data/${id}`)(dispatch);
+            }
+        }
+    });
+
+};
