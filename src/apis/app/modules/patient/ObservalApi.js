23a0a579cd3ee8e29d53700bf0fa3d6d62e316c8
Liangxiaojun <fatalxiao@163.com>
2018-03-02 10:43:23 +0800

    update

--- src/apis/app/modules/patient/ObservalApi.js
+++ src/apis/app/modules/patient/ObservalApi.js
@@ -3,6 +3,13 @@ import Api from 'apis/Api';
 
 export default {
 
+    getObservalDataByPatientId(options) {
+        Api.get({
+            ...options,
+            url: `${config.appBaseUrl}/observal/getObservalDataByPatientId/${options.params.patientId}`
+        });
+    },
+
     createOrUpdateObservalData(options) {
         Api.post({
             ...options,
