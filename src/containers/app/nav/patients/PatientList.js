bcc34fb3b99f7b60662001df8af9ad0060b1e161
Liangxiaojun <fatalxiao@163.com>
2018-03-05 10:19:10 +0800

    update

--- src/containers/app/nav/patients/PatientList.js
+++ src/containers/app/nav/patients/PatientList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import classNames from 'classnames';
 
 import * as actions from 'reduxes/actions';
 
@@ -14,7 +15,17 @@ import 'scss/containers/app/nav/patients/PatientList.scss';
 class PatientList extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.statusTouchTapHandler = ::this.statusTouchTapHandler;
+
+    }
+
+    statusTouchTapHandler(e, patientId) {
+
+        e.stopPropagation();
+
     }
 
     render() {
@@ -27,15 +38,29 @@ class PatientList extends Component {
                 {
                     $patientList.map((patient, index) => {
 
-                        const groupName = $groupList.find(item => item.id === patient.groupId).groupName,
+                        const patientId = patient.id,
+
+                            groupName = $groupList.find(item => item.id === patient.groupId).groupName,
+
+                            statusClassName = classNames('patient-status', {
+                                disabled: patient.disabled
+                            }),
+
                             {weeks, days} = Util.days2weeksDays(patient.gestationalDays);
 
                         return (
                             <FlatButton key={index}
                                         className="patient"
                                         onTouchTap={() => {
-                                            routerPush(`/app/patient/update-patient/${patient.id}`);
+                                            routerPush(`/app/patient/update-patient/${patientId}`);
                                         }}>
+
+                                <div className={statusClassName}
+                                     onTouchTap={e => {
+                                         this.statusTouchTapHandler(e, patientId);
+                                     }}>
+                                    <div className="patient-status-dot"></div>
+                                </div>
 
                                 <div className="patient-info">
                                     <span className="patient-name">{patient.patientName}</span>
@@ -43,7 +68,7 @@ class PatientList extends Component {
                                 </div>
 
                                 <div className="patient-desc">
-                                    {`${patient.id}  ·  ${weeks}w + ${days}d`}
+                                    {`${patientId}  ·  ${weeks}w + ${days}d`}
                                 </div>
 
                             </FlatButton>
