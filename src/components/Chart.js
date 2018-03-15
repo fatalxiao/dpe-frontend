b4e514fc41880eaecd6ec6ba875e11e8f15d0df5
Liangxiaojun <fatalxiao@163.com>
2018-03-15 14:17:16 +0800

    update

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Highcharts from 'highcharts';
+import highcharts from 'highcharts';
+import highchartsMore from 'highcharts/highcharts-more';
+import SolidGauge from 'highcharts/modules/solid-gauge';
 import classNames from 'classnames';
 
 import 'scss/components/FieldSet.scss';
+
+highchartsMore(highcharts);
+SolidGauge(highcharts);
 
 class Chart extends Component {
 
@@ -11,17 +16,21 @@ class Chart extends Component {
         super(props);
     }
 
+    componentDidMount() {
+        highcharts.chart(this.refs.chart, ...this.props.config);
+    }
+
     componentWillReceiveProps(nextProps) {
         if (nextProps.config) {
-            Highcharts.chart(this.refs.chart, ...config);
+            highcharts.chart(this.refs.chart, ...nextProps.config);
         }
     }
 
     render() {
 
-        const {className, config, ...restProps} = this.props,
+        const {className, ...restProps} = this.props,
 
-            chartClassName = className('chart', {
+            chartClassName = classNames('chart', {
                 [className]: className
             });
 
--- src/containers/app/modules/dashboard/Dashboard.js
+++ src/containers/app/modules/dashboard/Dashboard.js
@@ -2,18 +2,64 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import Highcharts from 'highcharts';
+
+import Chart from 'components/Chart';
 
 import * as actions from 'reduxes/actions/index';
 
 class Dashboard extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.config = [{
+            chart: {
+                type: 'solidgauge'
+            },
+            title: {
+                text: '22',
+                align: 'center',
+                verticalAlign: 'middle'
+            },
+            tooltip: {
+                enabled: false
+            },
+            pane: {
+                background: null
+            },
+            yAxis: {
+                min: 0,
+                max: 120,
+                lineWidth: 0,
+                tickPositions: []
+            },
+            plotOptions: {
+                solidgauge: {
+                    borderWidth: '10px',
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            },
+            series: [{
+                borderColor: '#f00',
+                data: [{
+                    radius: '100%',
+                    innerRadius: '100%',
+                    y: 22
+                }]
+            }]
+        }];
+
     }
 
     render() {
         return (
             <div className="dpe-list">
+
+                <Chart config={this.config}/>
 
             </div>
         );
