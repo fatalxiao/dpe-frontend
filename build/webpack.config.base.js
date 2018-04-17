74af1029e1fb98c2149d62ee8d0f8d40fd1d577c
liangxiaojun <fatalxiao@163.com>
2018-04-17 21:47:23 +0800

    update webpack build

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,23 +1,47 @@
-var path = require('path');
-var utils = require('./utils');
-var config = require('../config');
+const path = require('path'),
+    autoprefixer = require('autoprefixer'),
+
+    config = require('./config.js'),
+    utils = require('./utils.js'),
+
+    cssLoaderConfig = ['style-loader', {
+        loader: 'css-loader',
+        options: {
+            minimize: true,
+            importLoaders: 1
+        }
+    }, {
+        loader: 'postcss-loader',
+        options: {
+            ident: 'postcss',
+            plugins: [
+                autoprefixer({
+                    broswer: 'last 5 versions'
+                })
+            ]
+        }
+    }];
 
 function resolve(dir) {
     return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
+
     entry: {
         app: './src/index.js'
     },
+
     output: {
-        path: config.prod.assetsRoot,
+        path: config.production.assetsRoot,
         filename: '[name].js',
         publicPath: config.assetsPublicPath
     },
+
     resolve: {
         extensions: ['.js'],
         alias: {
+
             'src': resolve('src'),
             'apis': resolve('src/apis'),
             'assets': resolve('src/assets'),
@@ -28,28 +52,40 @@ module.exports = {
             'components': resolve('src/components'),
             'customized': resolve('src/customized'),
             'reduxes': resolve('src/reduxes'),
-            'vendors': resolve('src/vendors')
+            'vendors': resolve('src/vendors'),
+
+            'feedManager': resolve('src/assets/scss/containers/app/modules/feedManager'),
+            'biddingBudget': resolve('src/assets/scss/containers/app/modules/biddingBudget')
+
         }
     },
+
     module: {
         rules: [{
             test: /\.js$/,
             loader: 'babel-loader',
-            include: resolve('src')
+            include: [resolve('src'), resolve('node_modules/xml-name-validator/lib/xml-name-validator.js')]
         }, {
             test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
             loader: 'url-loader',
             query: {
                 limit: 1000,
-                name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                name: utils.assetsSubPath('img/[name].[hash:7].[ext]')
             }
         }, {
             test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
             loader: 'url-loader',
             query: {
                 limit: 1000,
-                name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                name: utils.assetsSubPath('fonts/[name].[hash:7].[ext]')
             }
+        }, {
+            test: /\.scss$/,
+            use: [...cssLoaderConfig, 'sass-loader']
+        }, {
+            test: /\.css$/,
+            use: cssLoaderConfig
         }]
     }
+
 };
