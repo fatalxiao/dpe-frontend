afd38a21a079ea22194e332cb4d03fbc1627e44f
liangxiaojun <fatalxiao@163.com>
2018-04-17 21:47:36 +0800

    update webpack build util

--- build/utils.js
+++ build/utils.js
@@ -1,78 +1,19 @@
 const fs = require('fs'),
     path = require('path'),
-    config = require('../config'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin');
+    crypto = require('crypto'),
 
-function assetsPath(_path) {
-    return path.posix.join(config.assetsSubDirectory, _path);
+    config = require('./config.js');
+
+function assetsPath(p, env = 'production') {
+    return path.posix.join(config[env].assetsDirectory, p);
 };
 
-function cssLoaders(options) {
-
-    options = options || {};
-
-    var cssLoader = {
-        loader: 'css-loader',
-        options: {
-            minimize: process.env.NODE_ENV === 'production',
-            sourceMap: options.sourceMap
-        }
-    };
-
-    // generate loader string to be used with extract text plugin
-    function generateLoaders(loader, loaderOptions) {
-
-        var loaders = [cssLoader];
-        if (loader) {
-            loaders.push({
-                loader: loader + '-loader',
-                options: Object.assign({}, loaderOptions, {
-                    sourceMap: options.sourceMap
-                })
-            });
-        }
-
-        // Extract CSS when that option is specified
-        // (which is the case during production build)
-        if (options.extract) {
-            return ExtractTextPlugin.extract({
-                use: loaders,
-                fallback: 'style-loader'
-            });
-        } else {
-            return ['style-loader'].concat(loaders);
-        }
-
-    }
-
-    // http://vuejs.github.io/vue-loader/en/configurations/extract-css.html
-    return {
-        css: generateLoaders(),
-        postcss: generateLoaders(),
-        less: generateLoaders('less'),
-        sass: generateLoaders('sass', {indentedSyntax: true}),
-        scss: generateLoaders('sass'),
-        stylus: generateLoaders('stylus'),
-        styl: generateLoaders('stylus')
-    };
-
+function assetsSubPath(p) {
+    return path.posix.join(config.assetsSubDirectory, p);
 };
 
-function styleLoaders(options) {
-
-    var output = [];
-    var loaders = exports.cssLoaders(options);
-
-    for (var extension in loaders) {
-        var loader = loaders[extension];
-        output.push({
-            test: new RegExp('\\.' + extension + '$'),
-            use: loader
-        });
-    }
-
-    return output;
-
+function assetsVendorsAbsolutePath(p, env = 'production') {
+    return path.posix.join(config[env].assetsRoot, assetsSubPath(`vendors/${p}`));
 };
 
 function fsExistsSync(path) {
@@ -166,11 +107,22 @@ function ipParse(ip) {
 
 }
 
+function calculateSHA256(filePath, callback) {
+    const rs = fs.createReadStream(filePath),
+        hash = crypto.createHash('sha256');
+    rs.on('data', hash.update.bind(hash));
+    rs.on('end', function () {
+        console.log('SHA-256 Hash: ', hash.digest('hex'), '\n');
+        callback && callback();
+    });
+}
+
 exports.assetsPath = assetsPath;
-exports.cssLoaders = cssLoaders;
-exports.styleLoaders = styleLoaders;
+exports.assetsSubPath = assetsSubPath;
+exports.assetsVendorsAbsolutePath = assetsVendorsAbsolutePath;
 exports.fsExistsSync = fsExistsSync;
 exports.copyRecursionSync = copyRecursionSync;
 exports.rmRecursionSync = rmRecursionSync;
 exports.getClientIp = getClientIp;
 exports.ipParse = ipParse;
+exports.calculateSHA256 = calculateSHA256;
