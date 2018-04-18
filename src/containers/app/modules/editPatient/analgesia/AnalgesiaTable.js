2e4763c09e073a506526ee19623258268b53d289
liangxiaojun <fatalxiao@163.com>
2018-04-18 16:49:59 +0800

    update AnalgesiaTable

--- src/containers/app/modules/editPatient/analgesia/AnalgesiaTable.js
+++ src/containers/app/modules/editPatient/analgesia/AnalgesiaTable.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import _ from 'lodash';
+import debounce from 'lodash/debounce';
 
 import * as actions from 'reduxes/actions/index';
 
@@ -39,7 +39,7 @@ class AnalgesiaTable extends Component {
 
     }
 
-    save = _.debounce(() => {
+    save = debounce(() => {
         const {patientId, createOrUpdateAnalgesiaData} = this.props;
         patientId && createOrUpdateAnalgesiaData(patientId, undefined, true);
     }, 250);
