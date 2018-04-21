edc01c7ad392e81c344a2191a068482cd2e1cbaa
liangxiaojun <fatalxiao@163.com>
2018-04-21 09:15:54 +0800

    update NavBarBottom

--- src/containers/app/nav/bar/NavBarBottom.js
+++ src/containers/app/nav/bar/NavBarBottom.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
-import * as actions from 'reduxes/actions';
-
 import 'scss/containers/app/nav/bar/NavBarBottom.scss';
 
 class NavBarBottom extends Component {
@@ -19,4 +17,4 @@ class NavBarBottom extends Component {
     }
 }
 
-export default connect(state => ({}), dispatch => bindActionCreators(actions, dispatch))(NavBarBottom);
+export default connect(state => ({}), dispatch => bindActionCreators({}, dispatch))(NavBarBottom);
