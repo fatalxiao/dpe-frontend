92e9d4fe5db4eb9182685e5e467879f87d322b7d
liangxiaojun <fatalxiao@163.com>
2018-06-14 20:29:09 +0800

    update dependences

--- src/index.js
+++ src/index.js
@@ -7,23 +7,14 @@ import createBrowserHistory from 'history/createBrowserHistory';
 import {renderRoutes} from 'react-router-config';
 import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import configureStore from 'reduxes/store';
 import {configureRoutes} from './config.routes';
 
 import 'scss/index.scss';
 
-// prevent console log in demo and production env
-if (process.env.NODE_ENV === 'production') {
-    console.log = () => {
-    };
-}
-
 const history = createBrowserHistory(),
     store = configureStore(history);
-
-injectTapEventPlugin();
 
 render(
     <Provider store={store}>
