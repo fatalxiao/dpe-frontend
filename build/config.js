9fb744dd44125924b3b0fe25430df5b4917ea69f
liangxiaojun <fatalxiao@163.com>
2018-10-06 22:29:27 +0800

    update build config

--- build/config.js
+++ build/config.js
@@ -3,27 +3,42 @@ const path = require('path');
 module.exports = {
 
     assetsPublicPath: '/',
-    assetsDirectory: 'dist',
     assetsSubDirectory: 'static',
     productionGzipExtensions: ['js', 'css'],
 
-    dev: {
-        port: 4001,
+    development: {
+        port: 4000,
         srcRoot: path.resolve(__dirname, '../src'),
         index: path.resolve(__dirname, '../src/index.html'),
         assetsVirtualRoot: path.posix.join('/', 'static'),
-        proxyTable: {},
-        cssSourceMap: false
-    },
-
-    build: {
-        index: path.resolve(__dirname, '../dist/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist'),
-        productionSourceMap: false
+        rootDirectory: 'dist/dist-dev',
+        assetsDirectory: 'dist/dist-dev/dist',
+        assetsRoot: path.resolve(__dirname, '../dist/dist-dev/dist'),
+        proxyTable: {
+            '/dpe': 'http://localhost:4100'
+        }
     },
 
     demo: {
-        port: 4002
+        port: 4001,
+        index: path.resolve(__dirname, '../dist/dist-demo/dist/index.html'),
+        rootDirectory: 'dist/dist-demo',
+        assetsDirectory: 'dist/dist-demo/dist',
+        assetsRoot: path.resolve(__dirname, '../dist/dist-demo/dist'),
+        proxyTable: {
+            '/dpe': 'http://localhost:4100'
+        }
+    },
+
+    production: {
+        port: 4002,
+        index: path.resolve(__dirname, '../dist/dist-prod/dist/index.html'),
+        rootDirectory: 'dist/dist-prod',
+        assetsDirectory: 'dist/dist-prod/dist',
+        assetsRoot: path.resolve(__dirname, '../dist/dist-prod/dist'),
+        proxyTable: {
+            '/dpe': 'http://localhost:4100'
+        }
     }
 
 };
