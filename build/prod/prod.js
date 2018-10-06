08ffe461d9e8e5d53b875509a260537f53a1b860
liangxiaojun <fatalxiao@163.com>
2018-10-06 22:05:36 +0800

    update build prod

--- build/prod/prod.js
+++ build/prod/prod.js
@@ -1,25 +1,11 @@
-const fs = require('fs'),
-    ora = require('ora'),
-    chalk = require('chalk'),
-    webpack = require('webpack'),
-    archiver = require('archiver'),
-    crypto = require('crypto'),
+const webpack = require('webpack'),
+    log = require('friendly-errors-webpack-plugin/src/output'),
 
-    config = require('../config.js'),
-    webpackConfig = require('./webpack.config.prod.js'),
-    {fsExistsSync, copyRecursionSync, rmRecursionSync} = require('../utils.js'),
+    webpackConfig = require('./webpack.config.prod.js');
 
-    env = process.env.NODE_ENV,
-    name = `dpe-frontend`,
-    path = `./${name}`,
-    zipPath = `./${name}.zip`,
-    spinner = ora('building for production...');
-
-spinner.start();
+log.title('info', 'WAIT', `Building Production... `);
 
 webpack(webpackConfig, (err, stats) => {
-
-    spinner.stop();
 
     if (err) {
         throw err;
@@ -33,47 +19,6 @@ webpack(webpackConfig, (err, stats) => {
         chunkModules: false
     }) + '\n\n');
 
-    // remove zip file
-    if (fsExistsSync(zipPath)) {
-        fs.unlinkSync(zipPath);
-    }
-
-    // remove temp dir
-    if (fsExistsSync(path)) {
-        rmRecursionSync(path);
-    }
-
-    // make temp dir
-    fs.mkdirSync(path);
-
-    // copy files
-    copyRecursionSync(config[env].rootDirectory, path, ['node_modules', '.DS_Store']);
-    copyRecursionSync('./release', path);
-
-    // make archive
-    const output = fs.createWriteStream(zipPath),
-        archive = archiver('zip', {zlib: {level: 9}});
-    output.on('close', () => {
-
-        console.log(chalk.cyan('Archive: ' + archive.pointer() + ' total bytes'));
-
-        // remove temp dir
-        if (fsExistsSync(path)) {
-            rmRecursionSync(path);
-        }
-
-        // calculate SHA-256 Hash
-        const rs = fs.createReadStream(zipPath),
-            hash = crypto.createHash('sha256');
-        rs.on('data', hash.update.bind(hash));
-        rs.on('end', function () {
-            console.log('SHA-256 Hash: ', hash.digest('hex'));
-            console.log(chalk.cyan('Build complete.'));
-        });
-
-    });
-    archive.pipe(output);
-    archive.directory(path, name);
-    archive.finalize();
+    log.title('success', 'DONE', `Build Complete `);
 
 });
