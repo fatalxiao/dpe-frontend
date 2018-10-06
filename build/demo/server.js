eca97dfb77ae7170651dcaef4e466f1551dbe1b9
liangxiaojun <fatalxiao@163.com>
2018-10-06 22:04:59 +0800

    update demo server

---
+++ build/demo/server.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+    history = require('connect-history-api-fallback'),
+    opn = require('opn'),
+    compression = require('compression'),
+    log = require('friendly-errors-webpack-plugin/src/output'),
+
+    config = require('../config.js'),
+
+    app = express(),
+    uri = 'http://localhost:' + config.demo.port;
+
+app
+.use(compression())
+.use(history())
+.use(express.static(config.build.assetsRoot, {
+    setHeaders: (res, path) => {
+        res.setHeader('Cache-Control', path.endsWith('index.html') ?
+            'no-cache, no-store, no_store, max-age=0, must-revalidate' : 'max-age=315360000'
+        );
+    }
+}))
+.listen(config.demo.port, err => {
+
+    if (err) {
+        log.title('error', 'ERROR', `${err} `);
+        return;
+    }
+
+    log.title('success', 'DONE', `Listening At ${uri} `);
+
+    opn(uri);
+
+});
