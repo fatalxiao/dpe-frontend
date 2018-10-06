919a1c148c006915476dddcc6accb9c62fb81846
liangxiaojun <fatalxiao@163.com>
2018-10-06 22:28:21 +0800

    update webpack dev config

--- build/dev/webpack.config.dev.js
+++ build/dev/webpack.config.dev.js
@@ -8,7 +8,7 @@ const webpack = require('webpack'),
     env = process.env.NODE_ENV;
 
 Object.keys(baseWebpackConfig.entry).forEach(name => {
-    baseWebpackConfig.entry[name] = ['./build/dev/dev-client'].concat(baseWebpackConfig.entry[name]);
+    baseWebpackConfig.entry[name] = ['./build/dev/client'].concat(baseWebpackConfig.entry[name]);
 });
 
 module.exports = merge(baseWebpackConfig, {
@@ -16,6 +16,10 @@ module.exports = merge(baseWebpackConfig, {
     mode: 'development',
 
     devtool: '#cheap-module-eval-source-map',
+
+    optimization: {
+        noEmitOnErrors: true
+    },
 
     plugins: [
 
@@ -27,13 +31,12 @@ module.exports = merge(baseWebpackConfig, {
 
         new webpack.HotModuleReplacementPlugin(),
 
-        new webpack.NoEmitOnErrorsPlugin(),
-
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html',
             favicon: './src/assets/images/favicon.ico',
-            inject: true
+            inject: true,
+            chunksSortMode: 'none'
         }),
 
         new FriendlyErrorsPlugin()
